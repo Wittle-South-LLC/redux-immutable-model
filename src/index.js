578036bb00edcb5460a1740b35b743f0b5a30af8
Eric Wittle <eric@wittle.net>
2019-01-19 07:55:08 -0500

    Fixing module exports so that this works as designed.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import BaseRIMObject from './BaseRIMObject'
+import BaseRIMService from './BaseRIMService'
+import Configuration from './Configuration'
+import defaultVerbs from './ReduxVerbs'
+
+module.exports = {
+  BaseRIMObject: BaseRIMObject,
+  BaseRIMService: BaseRIMService,
+  Configuration: Configuration,
+  defaultVerbs: defaultVerbs
+}
